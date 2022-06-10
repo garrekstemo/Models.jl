bbb7ada38ea0eaabc5dd7cbf08442ecb88bcc1a1
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2022-06-10 17:31:20 +0900

    Broadcast arguments

--- src/Models.jl
+++ src/Models.jl
@@ -1,11 +1,13 @@
 module Models
 
-function exponential(x, f0, τ)
-    return f0 * exp(- x / τ)
+function exponential(Float64::x, Vector{Float64}::p)
+    f0, τ, y0 = p
+    return @. f0 * exp(- x / τ) + y0
 end
 
-function double_exponential(x, f1, τ1, f2, τ2)
-    return exponential(x, f1, τ1) + exponential(x, f2, τ2)
+function double_exponential(Float64::x, Vector{Float64}::p)
+    f1, τ1, f2, τ2, y0 = p
+    return @. f1 * exp(- x / τ1) + f2 * exp(- x / τ2) + y0
 end
 
 end # module
