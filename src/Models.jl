7a58c77448a2054bd3bd2bfa76912d1628eb8768
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2022-06-10 17:33:13 +0900

    Fixed argument error

--- src/Models.jl
+++ src/Models.jl
@@ -1,11 +1,11 @@
 module Models
 
-function exponential(Float64::x, Vector{Float64}::p)
+function exponential(x::Float64, p::Vector{Float64})
     f0, τ, y0 = p
     return @. f0 * exp(- x / τ) + y0
 end
 
-function double_exponential(Float64::x, Vector{Float64}::p)
+function double_exponential(x::Float64, p::Vector{Float64})
     f1, τ1, f2, τ2, y0 = p
     return @. f1 * exp(- x / τ1) + f2 * exp(- x / τ2) + y0
 end
